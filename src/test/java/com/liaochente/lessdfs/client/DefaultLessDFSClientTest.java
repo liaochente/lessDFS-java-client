ab02a22967e217ac0910dfebae983be004b98a4f
liao_chente <liao_chente@hopedove.com>
2020-05-18 17:32:28 +0800

    test: 增加测试用例

---
+++ src/test/java/com/liaochente/lessdfs/client/DefaultLessDFSClientTest.java
@@ -0,0 +1,32 @@
+package com.liaochente.lessdfs.client;
+
+import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class DefaultLessDFSClientTest {
+
+    @Test
+    public void upload() throws FileNotFoundException {
+        DefaultLessDFSClient client = DefaultLessDFSClient.newInstance();
+        String fileName = client.upload("/Users/liaochente/CSS禅意花园.pdf", "pdf");
+        assertNotNull(fileName);
+    }
+
+    @Test
+    public void download() throws Exception {
+        DefaultLessDFSClient client = DefaultLessDFSClient.newInstance();
+        InputStream inputStream = client.download("L0/b641807e48ae47d985defebe7c73c145");
+        assertNotNull(inputStream);
+    }
+
+    @Test
+    public void delete() throws Exception {
+        DefaultLessDFSClient client = DefaultLessDFSClient.newInstance();
+        boolean success = client.delete("L0/b641807e48ae47d985defebe7c73c145");
+        assertTrue(success);
+    }
+}
